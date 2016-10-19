c9ceb0ada148ceb85c9993dfbb4a86f966498286
ChrisGaddes <cagaddes@gmail.com>
2016-10-19 17:44:54 -0500

    Removed SwipeStack library due to low performance, replaced with SwipeDeck2 library: https://github.com/aaronbond/SwipeDeck2

--- app/src/main/java/com/chrisgaddes/focus/MainActivity.java
+++ app/src/main/java/com/chrisgaddes/focus/MainActivity.java
@@ -1,43 +1,154 @@
-
 package com.chrisgaddes.focus;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.daprlabs.aaron.swipedeck.SwipeDeck;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button btn_load_second_activity;
+    private static final String TAG = "MainActivity";
+    private SwipeDeck cardStack;
+    private Context context = this;
+    private SwipeDeckAdapter adapter;
+    private ArrayList<String> testData;
+    private CheckBox dragCheckbox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cardStack = (SwipeDeck) findViewById(R.id.swipe_deck);
+        dragCheckbox = (CheckBox) findViewById(R.id.checkbox_drag);
 
-        btn_load_second_activity = (Button) findViewById(R.id.btn_load_second_activity);
-        btn_load_second_activity.setOnClickListener(new View.OnClickListener() {
+        testData = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            testData.add(String.valueOf(i));
+        }
+
+        adapter = new SwipeDeckAdapter(testData, this);
+        if(cardStack != null){
+            cardStack.setAdapter(adapter);
+        }
+        cardStack.setCallback(new SwipeDeck.SwipeDeckCallback() {
             @Override
-            public void onClick(View v) {
-                loadSecondActivity();
+            public void cardSwipedLeft(long stableId) {
+                Log.i("MainActivity", "card was swiped left, position in adapter: " + stableId);
+            }
+
+            @Override
+            public void cardSwipedRight(long stableId) {
+                Log.i("MainActivity", "card was swiped right, position in adapter: " + stableId);
+
+            }
+
+            //            @Override
+            public boolean isDragEnabled(long itemId) {
+                return dragCheckbox.isChecked();
             }
         });
 
-//        SwipeStack swipeStack = (SwipeStack) findViewById(R.id.swipeStack);
-//        swipeStack.setAdapter(new SwipeStackAdapter(mData));
+        cardStack.setLeftImage(R.id.left_image);
+        cardStack.setRightImage(R.id.right_image);
+
+        Button btn = (Button) findViewById(R.id.button_left);
+        btn.setOnClickListener(this);
+
+        Button btn2 = (Button) findViewById(R.id.button_right);
+        btn2.setOnClickListener(this);
+
+        Button btn3 = (Button) findViewById(R.id.button_center);
+        btn3.setOnClickListener(this);
 
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
 
-    private void loadSecondActivity() {
-//        Intent intent = new Intent(this, RecyclerViewDisplayActivity.class);
+            case R.id.button_left:
+                cardStack.swipeTopCardLeft(500);
+                break;
 
-//        intent.putExtra(Constant.LAYOUT_MANAGER, Constant.LINEAR_LAYOUT_MANAGER);
+            case R.id.button_right:
+                cardStack.swipeTopCardRight(180);
+                break;
 
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+            case R.id.button_center:
+                cardStack.unSwipeCard();
+                break;
+
+            default:
+                break;
+        }
     }
 
+    public class SwipeDeckAdapter extends BaseAdapter {
+
+        private List<String> data;
+        private Context context;
+
+        public SwipeDeckAdapter(List<String> data, Context context) {
+            this.data = data;
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return data.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return data.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+
+            View v = convertView;
+            if (v == null) {
+                LayoutInflater inflater = getLayoutInflater();
+                // normally use a viewholder
+                v = inflater.inflate(R.layout.test_card2, parent, false);
+            }
+            //((TextView) v.findViewById(R.id.textView2)).setText(data.get(position));
+            ImageView imageView = (ImageView) v.findViewById(R.id.offer_image);
+            Picasso.with(context).load(R.drawable.food).fit().centerCrop().into(imageView);
+            TextView textView = (TextView) v.findViewById(R.id.sample_text);
+            String item = (String)getItem(position);
+            textView.setText(item);
+
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i("Layer type: ", Integer.toString(v.getLayerType()));
+                    Log.i("Hardware Accel type:", Integer.toString(View.LAYER_TYPE_HARDWARE));
+                    /*Intent i = new Intent(v.getContext(), BlankActivity.class);
+                    v.getContext().startActivity(i);*/
+                }
+            });
+            return v;
+        }
+    }
 }
