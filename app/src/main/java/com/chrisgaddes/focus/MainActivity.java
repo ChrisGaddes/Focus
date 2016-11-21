ffc6e463f86cdddf7584209fcedd6f81b76bdf67
Chris Gaddes <chrisgaddesdev@gmail.com>
2016-11-20 22:20:00 -0600

    Started over

--- app/src/main/java/com/chrisgaddes/focus/MainActivity.java
+++ app/src/main/java/com/chrisgaddes/focus/MainActivity.java
@@ -1,60 +1,142 @@
 package com.chrisgaddes.focus;
-import android.content.Context;
-import android.graphics.Point;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
 
-import com.mindorks.placeholderview.SwipeDecor;
-import com.mindorks.placeholderview.SwipePlaceHolderView;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.daprlabs.aaron.swipedeck.SwipeDeck;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SwipePlaceHolderView mSwipeView;
-    private Context mContext;
+    private SwipeDeck cardStack;
+    private ArrayList testData;
+    private SwipeDeckAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSwipeView = (SwipePlaceHolderView)findViewById(R.id.swipeView);
-        mContext = getApplicationContext();
+        cardStack = (SwipeDeck) findViewById(R.id.swipe_deck);
 
-        int bottomMargin = Utils.dpToPx(160);
-        Point windowSize = Utils.getDisplaySize(getWindowManager());
-        mSwipeView.getBuilder()
-                .setDisplayViewCount(3)
-                .setHeightSwipeDistFactor(10)
-                .setWidthSwipeDistFactor(5)
-                .setSwipeDecor(new SwipeDecor()
-                        .setViewWidth(windowSize.x)
-                        .setViewHeight(windowSize.y - bottomMargin)
-                        .setViewGravity(Gravity.TOP)
-                        .setSwipeRotationAngle(0)
-                        .setPaddingTop(20)
-                        .setRelativeScale(0.01f)
-                        .setSwipeInMsgLayoutId(R.layout.tinder_swipe_in_msg_view)
-                        .setSwipeOutMsgLayoutId(R.layout.tinder_swipe_out_msg_view));
+        testData = new ArrayList<>();
+        testData.add("0");
+        testData.add("1");
+        testData.add("2");
+        testData.add("3");
+        testData.add("4");
+
+        adapter = new SwipeDeckAdapter(testData, this);
+        if (cardStack != null) {
+            cardStack.setAdapter(adapter);
+        }
+        cardStack.setCallback(new SwipeDeck.SwipeDeckCallback() {
+            @Override
+            public void cardSwipedLeft(long stableId) {
+                Log.i("MainActivity", "card was swiped left, position in adapter: " + stableId);
+            }
+
+            @Override
+            public void cardSwipedRight(long stableId) {
+                Log.i("MainActivity", "card was swiped right, position in adapter: " + stableId);
+            }
+        });
+
+        cardStack.setLeftImage(R.id.left_image);
+        cardStack.setRightImage(R.id.right_image);
+
+        //example of buttons triggering events on the deck
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cardStack.swipeTopCardLeft(180);
+            }
+        });
+        Button btn2 = (Button) findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cardStack.swipeTopCardRight(180);
+            }
+        });
+
+        Button btn3 = (Button) findViewById(R.id.button3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                testData.add("a sample string.");
+                adapter.notifyDataSetChanged();
+            }
+        });
 
 
-        for(Profile profile : Utils.loadProfiles(this.getApplicationContext())){
-            mSwipeView.addView(new TinderCard(mContext, profile, mSwipeView));
+
+
+    }
+
+    public class SwipeDeckAdapter extends BaseAdapter {
+
+        private List<String> data;
+        private Context context;
+
+        public SwipeDeckAdapter(List<String> data, Context context) {
+            this.data = data;
+            this.context = context;
         }
 
-        findViewById(R.id.rejectBtn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mSwipeView.doSwipe(false);
-            }
-        });
+        @Override
+        public int getCount() {
+            return data.size();
+        }
 
-        findViewById(R.id.acceptBtn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mSwipeView.doSwipe(true);
+        @Override
+        public Object getItem(int position) {
+            return data.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+
+            View v = convertView;
+            if (v == null) {
+                LayoutInflater inflater = getLayoutInflater();
+                // normally use a viewholder
+                v = inflater.inflate(R.layout.test_card2, parent, false);
             }
-        });
+
+            ImageView imageView = (ImageView) v.findViewById(R.id.offer_image);
+            Picasso.with(context).load(R.drawable.food).fit().centerCrop().into(imageView);
+            TextView textView = (TextView) v.findViewById(R.id.sample_text);
+            String item = (String)getItem(position);
+            textView.setText(item);
+
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i("Layer type: ", Integer.toString(v.getLayerType()));
+                    Log.i("Hardware Accel type:", Integer.toString(View.LAYER_TYPE_HARDWARE));
+                }
+            });
+            return v;
+        }
     }
+
 }
